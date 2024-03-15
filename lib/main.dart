9cc5b4aad3a09b2b9d3bf07345e9330203964fbc
LeSac <107132448+lesacIT@users.noreply.github.com>
2024-03-15 14:35:47 +0700

    update cấu trúc code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import './pages/login.dart';
-// import './pages/home.dart';
+// import './pages/login.dart';
+import './pages/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +10,6 @@ void main() async {
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Login(),
+    home: HomePage(),
   ));
 }
